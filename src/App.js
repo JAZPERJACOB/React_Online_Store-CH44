75c4756a4f3649f5069ad42bcc5fe1f8577c1624
Jazper Jacob <jazperjacob@Jazpers-MBP.lan>
2024-02-15 19:46:36 -0800

    added routes on Navbar

--- src/App.js
+++ src/App.js
@@ -8,20 +8,26 @@ import About from './pages/about';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return (  
+    <BrowserRouter>
     <div className="App">
       <h1>Hello from React!</h1>
         <Navbar></Navbar>
-        <Home/>
-        <Catalog></Catalog>
-        <About/>
+        
+        <Routes>
+          <Route path="/" element={ <Home/>} />
+          <Route path="/" element={ <About/>}/>
+          <Route path="/catalog" element={ <Catalog/>}/>
+        </Routes>
+
         <Footer></Footer>
 
     </div>
+    </BrowserRouter>
   );
 }
 
